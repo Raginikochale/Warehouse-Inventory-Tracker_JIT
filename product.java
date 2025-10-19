d7e893c3c5b04234b8781d24266b5dcd7f394d32
Ragini Kohale <146700609+Raginikochale@users.noreply.github.com>
2025-10-19 21:08:04 +0530

    Update product.java

--- product.java
+++ product.java
@@ -1,6 +1,54 @@
+package main.java.warehouse;
+
 public class Product {
-  public static void main(String[] args) {
-    int data = 0;
-    
-  }
+        private final String id;
+        private final String name;
+        private int quantity;
+        private final int reorderThreshold;
+
+        public Product(String id, String name, int quantity, int reorderThreshold) {
+            this.id = id;
+            this.name = name;
+            this.quantity = quantity;
+            this.reorderThreshold = reorderThreshold;
+        }
+
+        // Getters
+        public String getId() {
+            return id;
+        }
+        public String getName() {
+            return name;
+        }
+        public int getQuantity() {
+            return quantity;
+        }
+        public int getReorderThreshold() {
+            return reorderThreshold;
+        }
+
+        // Setters
+        public void setQuantity(int quantity) {
+            this.quantity = quantity;
+        }
+        
+        public void increaseQuantity(int amount) {
+            if(amount > 0) {
+                quantity += amount;
+            }
+        }
+        
+        public void decreaseQuantity(int amount) throws IllegalAccessException  {
+            if(amount > 0 && amount <= quantity) {
+                quantity -= amount;
+            } else {
+                throw new IllegalAccessException("Insufficient stock for " +name);
+            }
+        }
+
+        @Override
+        public String toString() {
+            return "Product{" + "ID='" +id+ '\'' + ", Name='" +name+ '\'' + ", Quantity=" +quantity+ ", Threshold=" +reorderThreshold+ '}';
+        }
+        
 }
