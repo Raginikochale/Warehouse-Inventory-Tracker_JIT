e138357fc96f6157b9207b522dafe7e2ce83127a
Ragini Kohale <146700609+Raginikochale@users.noreply.github.com>
2025-10-25 13:15:44 +0530

    Update Product.java

--- Product.java
+++ Product.java
@@ -1,52 +1,45 @@
 public class Product {
-        private final String id;
-        private final String name;
-        private int quantity;
-        private final int reorderThreshold;
+    private final String id;
+    private final String name;
+    private int quantity;
+    private final int reorderThreshold;
 
-        public Product(String id, String name, int quantity, int reorderThreshold) {
-            this.id = id;
-            this.name = name;
-            this.quantity = quantity;
-            this.reorderThreshold = reorderThreshold;
-        }
+    public Product(String id, String name, int quantity, int reorderThreshold) {
+        this.id = id;
+        this.name = name;
+        this.quantity = quantity;
+        this.reorderThreshold = reorderThreshold;
+    }
 
-        // Getters
-        public String getId() {
-            return id;
-        }
-        public String getName() {
-            return name;
-        }
-        public int getQuantity() {
-            return quantity;
-        }
-        public int getReorderThreshold() {
-            return reorderThreshold;
-        }
+    public String getId() {
+        return id;
+    }
 
-        // Setters
-        public void setQuantity(int quantity) {
-            this.quantity = quantity;
-        }
-        
-        public void increaseQuantity(int amount) {
-            if(amount > 0) {
-                quantity += amount;
-            }
-        }
-        
-        public void decreaseQuantity(int amount) throws IllegalAccessException  {
-            if(amount > 0 && amount <= quantity) {
-                quantity -= amount;
-            } else {
-                throw new IllegalAccessException("Insufficient stock for " +name);
-            }
-        }
+    public String getName() {
+        return name;
+    }
 
-        @Override
-        public String toString() {
-            return "Product{" + "ID='" +id+ '\'' + ", Name='" +name+ '\'' + ", Quantity=" +quantity+ ", Threshold=" +reorderThreshold+ '}';
+    public int getQuantity() {
+        return quantity;
+    }
+
+    public int getReorderThreshold() {
+        return reorderThreshold;
+    }
+
+    public void increaseQuantity(int amount) {
+        quantity += amount;
+    }
+
+    public void decreaseQuantity(int amount) throws IllegalArgumentException {
+        if (amount > quantity) {
+            throw new IllegalArgumentException("Insufficient stock for product: " + name);
         }
-        
+        quantity -= amount;
+    }
+
+    @Override
+    public String toString() {
+        return name + " (ID: " + id + ", Qty: " + quantity + ")";
+    }
 }
